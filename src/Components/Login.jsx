129ea57bb9341f2a6077e1f82ec03392ce4c39a1
Dhyey77-debug <24CE113@CHARUSAT.EDU.IN>
2026-01-29 10:51:58 +0530

    Comments addded

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -14,45 +14,49 @@ function Login() {
       
       {/* IMAGE PANEL */}
       <div className="auth-image">
-        <img src={login_image} alt="Illustration" />
+        <img src={login_image} alt="Illustration" /> //left-side image
       </div>
 
       {/* FORM PANEL */}
-      <div className="auth-form">
+      <div className="auth-form"> //right-side form
         <h1>{isRegister ? "Create an Account" : "Sign in to Continue"}</h1>
 
-        <button className="social-btn">
+        <button className="social-btn"> // Google sign-in button
           <img src={Google_logo} />
           {isRegister ? "Sign up with Google" : "Sign in with Google"}
         </button>
 
-        <button className="social-btn">
+        <button className="social-btn"> // Facebook sign-in button
           <img src={Facebook_logo} />
           {isRegister ? "Sign up with Facebook" : "Sign in with Facebook"}
         </button>
 
-        <button className="social-btn">
+        <button className="social-btn">// Apple sign-in button
           <img src={Apple_logo} />
           {isRegister ? "Sign up with Apple" : "Sign in with Apple"}
         </button>
 
-        <div className="or-divider">
+        <div className="or-divider"> //or divider ---------- or ----------
           <span></span>
           <p>or</p>
           <span></span>
         </div>
 
+        //if you are in register then text field for full name will be shown
         {isRegister && (
           <input type="text" placeholder="Full name" />
         )}
 
+        //input fields for email and password
         <input type="email" placeholder="Email address" />
         <input type="password" placeholder="Password" />
 
+        //submit button
         <button className="submit-btn" onClick={() => navigate("/")}>
           {isRegister ? "Register" : "Sign in"}
         </button>
 
+        //toggle between register and login
         <p className="switch-text" onClick={() => setIsRegister(!isRegister)}>
           {isRegister
             ? "Already have an account? Sign in"
